ffd0439805e9b40480c3fd4cd5c77d26f08eb0e4
haroldevs <haroldthomasg18@gmail.com>
2022-09-28 19:49:29 -0500

    js

---
+++ javascript/semana5.js/variables.js
@@ -0,0 +1,14 @@
+//* var
+//* let
+//* const
+
+
+//* var no se puede usar como variables porque esta reservada en js
+var n1=1;
+n1="hola";
+var n2=10;
+//! var ya no se usa
+// var es global
+// var es reasignable
+// var es pesado
+var n1=true;
