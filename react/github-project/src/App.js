8665ae7eb7e0d0e991f7b727848ab2d9ada47596
haroldevs <haroldthomasg18@gmail.com>
2022-10-26 23:17:20 -0500

    agregar api

--- react/github-project/src/App.js
+++ react/github-project/src/App.js
@@ -11,12 +11,25 @@ import octocat from "./assets/icons/Octocat.png"
 
 function App() {
   const [inputText,setInputText]=useState("")
+  const [user,setUser]=useState(null)
+
 
   function handleInputChange(event){
     setInputText(event.target.value)
   }
 
-  function searchUser(){
+  async function searchUser(){
+    try {
+      const response=await fetch(`https://api.github.com/users/${inputText}`)
+      const data=await response.json()
+      setUser(data)
+      console.log("data",data)
+
+    } catch (error) {
+      console.log("Error",error)
+    }
+
+
     console.log(inputText)
   }
 
@@ -36,7 +49,7 @@ function App() {
             onChange={handleInputChange}
             className="input-search"
             type="text"
-            placeholder="Search GitHub username..." autocomplete="off" id="usergithub"
+            placeholder="Search GitHub username..." autoComplete="off" id="usergithub"
           />
         </div>
         <div>
@@ -45,51 +58,51 @@ function App() {
       </div>
       <div className="information-container" id="container">
         <div className="image-container" id="imagen">
-          <img width="100" src={octocat} alt="" />
+          <img width="100" src={user?.avatar_url} alt="" />
         </div>
         <div className="description-container">
           <div className="user-date-container" >
-            <h2 id="name">The Octocat</h2>
-            <p id="created-at">Joined 25 Jun 2011</p>
+            <h2 id="name">{user?.name}</h2>
+            <p id="created-at">{user?.created_at}</p>
           </div>
           <div className="user-bio-container">
-            <p id="login">@octocat</p>
-            <p id="bio">This profile has no bio</p>
+            <p id="login">@{user?.login}</p>
+            <p id="bio">{user?.bio}</p>
           </div>
           <div className="card-information">
             <div>
               <h5>Repos</h5>
-              <h2 id="public_repos">8</h2>
+              <h2 id="public_repos">{user?.public_repos}</h2>
             </div>
             <div>
               <h5>Followers</h5>
-              <h2 id="followers">3938</h2>
+              <h2 id="followers">{user?.followers}</h2>
             </div>
             <div>
               <h5>Following</h5>
-              <h2 id="following">9</h2>
+              <h2 id="following">{user?.following}</h2>
             </div>
           </div>
           <div className="info-container">
             <div>
               <p id="location">
                 <img width="15" src={pin} alt="" />&nbsp;
-                San Francisco
+                {user?.location}
               </p>
               <p id="blog">
                 <img width="15" src={link} alt="" />&nbsp;
-                https://github.blog
+                {user?.blog}
               </p>
             </div>
             <div>
               <p id="twitter_username">
                 <img width="15" src={twitter} alt="" />&nbsp;
-                Not Available
+                {user?.twitter_username}
               </p>
 
               <p id="company">
                 <img width="15" src={hotel} alt="" />&nbsp;
-                @github
+                {user?.company}
               </p>
             </div>
           </div>
